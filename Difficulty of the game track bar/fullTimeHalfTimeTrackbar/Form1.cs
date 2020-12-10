e997ceca0229a2db14f0f357f6498c2f2dad1c17
ua4aeu <ra4a@yandex.ru>
2020-12-10 13:22:56 +0300

    Add files via upload

---
+++ Difficulty of the game track bar/fullTimeHalfTimeTrackbar/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fullTimeHalfTimeTrackbar
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWhatStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trkCredits_Scroll(object sender, EventArgs e)
+        {
+            int credits = trkCredits.Value;
+
+            //Показать значение трекбара в lblCredits
+            lblCredits.Text = trkCredits.Value.ToString();
+
+            //Вычисление состояния и отображение в lblStatus
+            if (credits >= 12)
+            {
+                lblStatus.Text = "Сложная игра";
+            }
+            else if (credits >= 6)
+            {
+                lblStatus.Text = "Средняя сложность";
+            }
+            else
+            {
+                lblStatus.Text = "Простая игра";
+            }
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
